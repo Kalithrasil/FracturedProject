bd2a8e8df545fac71dbe5bea3309aaf70015a90b
JUSTIN JOUBERT <PROTOSS.JUSTIN@GMAIL.COM>
2016-02-18 17:15:29 +0400

    Initial Project Setup

---
+++ Source/FracturedProject/FracturedProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FracturedProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FracturedProject, "FracturedProject" );
---
+++ Source/FracturedProject/FracturedProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FracturedProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRACTUREDPROJECT_API AFracturedProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
