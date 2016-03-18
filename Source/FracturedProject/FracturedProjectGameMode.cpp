8f0ad5aed7da5f1a8bb16b932a40ba5e9ac6b193
JUSTIN JOUBERT <PROTOSS.JUSTIN@GMAIL.COM>
2016-03-18 12:53:51 +0400

    Forced restriction of server to two players, all others are rejected No way to reset this yet without restarting the server

--- Source/FracturedProject/FracturedProjectGameMode.cpp
+++ Source/FracturedProject/FracturedProjectGameMode.cpp
@@ -3,6 +3,23 @@
 #include "FracturedProject.h"
 #include "FracturedProjectGameMode.h"
 
+void AFracturedProjectGameMode::PreLogin(const FString & Options, const FString & Address, const TSharedPtr< const FUniqueNetId > & UniqueId, FString & ErrorMessage)
+{
+	if (AFracturedProjectGameMode::player_amount >= 2)
+	{
+		ErrorMessage = "Server is full, cannot join Server.";
+		UE_LOG(LogTemp, Warning, TEXT("Player was rejected"));
+	}
+	else
+	{
+		Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+		UE_LOG(LogTemp, Warning, TEXT("Player was accepted"));
+	}
 
+}
 
-
+void AFracturedProjectGameMode::PostLogin(APlayerController * NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	AFracturedProjectGameMode::player_amount++;
+}
--- Source/FracturedProject/FracturedProjectGameMode.h
+++ Source/FracturedProject/FracturedProjectGameMode.h
@@ -12,8 +12,10 @@ UCLASS()
 class FRACTUREDPROJECT_API AFracturedProjectGameMode : public AGameMode
 {
 	GENERATED_BODY()
+
+		int player_amount = 0;
+
+		virtual void PreLogin(const FString & Options, const FString & Address, const TSharedPtr< const FUniqueNetId > & UniqueId, FString & ErrorMessage) override;
 	
-	
-	
-	
+		virtual void PostLogin(APlayerController * NewPlayer) override;
 };
